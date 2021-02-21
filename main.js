d00a08132a6c18334747377245b984419f73dedf
Srđan Margetić <srdjan@stuntcoders.com>
2021-02-21 03:01:06 +0100

    Add logic for the hamburger menu moving

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
+const video = document.querySelector('.header__video');
+const textBox = document.querySelector('.header__text-box');
+const main = document.querySelector('main');
+const footer = document.querySelector('.footer');
 
-toggle.addEventListener('click', ()=> toggle.classList.toggle('close'));
+toggle.addEventListener('click', function() {
+    toggle.classList.toggle('close');
+    menu.classList.toggle('move-left');
+    video.classList.toggle('move-left');
+    textBox.classList.toggle('move-left');
+    main.classList.toggle('move-left');
+    footer.classList.toggle('move-left');
+});
