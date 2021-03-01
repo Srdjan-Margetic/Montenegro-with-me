328ff402fad8ebe7666e97d9fd8453dd9f2b6238
Srđan Margetić <srdjan@stuntcoders.com>
2021-03-01 11:28:44 +0100

    Add logic for API

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const video = document.querySelector('.header__video');
 const textBox = document.querySelector('.header__text-box');
 const main = document.querySelector('main');
 const footer = document.querySelector('.footer');
+const footerRight = document.querySelector('.footer__content--right');
 
 toggle.addEventListener('click', function() {
     toggle.classList.toggle('close');
@@ -13,3 +14,38 @@ toggle.addEventListener('click', function() {
     main.classList.toggle('move-left');
     footer.classList.toggle('move-left');
 });
+
+const renderCountry = function(data, className = '' ) {
+    const html = `
+    <article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <h3 class="country__name">${data.name}</h3>
+        <div class="info-side"> 
+            <p class="time__zone"><span>⏱</span> ${data.timezones[0]}</p>
+            <p class="population"><span>👫 </span> ${ +(data.population / 1000000).toFixed(1)} mil. people</p>
+            <p class="language"><span>🗣️</span> Montenegrian</p>
+            <p class="currency"><span>💰</span> ${data.currencies[0].name}</p>
+        </div>
+    </div>
+</article> `
+    footerRight.insertAdjacentHTML('beforeend', html);
+}
+
+const renderError = function(msg) {
+    footerRight.insertAdjacentText('beforeend', msg);
+}
+
+const getCountryData = function(country) {
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+        renderCountry(data[0]);
+    })
+    .catch(err => {
+        renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
+        console.error(`${err} 💥💥💥`);
+    })
+}
+
+getCountryData('montenegro', 'montenegro__data');
+
